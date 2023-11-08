7a9cf8ecb551ddd93e9d445f7ab998571c138ef5
HypsterOP <indiangaming3000@gmail.com>
2023-11-08 21:38:43 +0530

    ok

--- songs-thing/main.py
+++ songs-thing/main.py
@@ -16,8 +16,7 @@ token_dict = oauth_object.get_access_token()
 token = token_dict['access_token'] 
 spotifyObject = spotipy.Spotify(auth=token) 
 user_name = spotifyObject.current_user() 
-  
-# To print the response in readable format. 
+ 
 print(json.dumps(user_name, sort_keys=True, indent=4)) 
 
 
